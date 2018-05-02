9f96dcc8e8bfb0a7c5f93a24d63cb81b2b7ba568
Ricardo <rferro@hexacta.com>
2018-05-02 00:02:07 -0300

    first commit

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.modal-trigger').leanModal();
+    $('#textarea1').val('New Text');
+  	$('#textarea1').trigger('autoresize');
+  	$('#someTab').tab('show');
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
