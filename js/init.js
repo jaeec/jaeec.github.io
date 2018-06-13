57010bb33ae4a560f9032557210d9fe3265a40af
Ricardo <rferromoreno@gmail.com>
2018-06-13 01:59:18 -0300

    Fix responsive navbar

--- js/init.js
+++ js/init.js
@@ -1,6 +1,17 @@
+/*
 $(document).ready(function(){
   $('.sidenav-trigger').sideNav();
   $('.parallax').parallax();
   $('.carousel').carousel();
   $('.tabs').tabs();
 });
+*/
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.tabs').tabs();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
