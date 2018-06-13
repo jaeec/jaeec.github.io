b8c6722dd91d4ff262f8c135edc166085ba9e098
Ricardo <rferromoreno@gmail.com>
2018-06-13 00:22:34 -0300

    Cambios

--- js/init.js
+++ js/init.js
@@ -1,7 +1,6 @@
-(function($){
-  $(function(){
-    $('.parallax').parallax();
-    $('.carousel').carousel();
-    $('.tabs').tabs();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  $('.parallax').parallax();
+  $('.carousel').carousel();
+  $('.tabs').tabs();
+});
