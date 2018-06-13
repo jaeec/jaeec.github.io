7bb0c78c8dd65f0496529c9ca83756615774c3c3
Ricardo <rferromoreno@gmail.com>
2018-06-12 23:56:10 -0300

    Remove cosas obsoletas

--- js/init.js
+++ js/init.js
@@ -2,10 +2,5 @@
   $(function(){
     $('.parallax').parallax();
     $('.carousel').carousel();
-    $('.modal-trigger').leanModal();
-    $('#textarea1').val('New Text');
-  	$('#textarea1').trigger('autoresize');
-  	$('#someTab').tab('show');
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
